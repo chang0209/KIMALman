394ef295ae5368c422d5a684707327f81d5a33f3
chang0209 <chang@kyonggi.ac.kr>
2026-06-12 09:31:31 +0900

    Update setup script: re-run dependency install (v2.3)

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# 필수 라이브러리 설치 (manga-ocr 라이브러리가 진짜 핵심입니다!)
+# 필수 라이브러리 설치 (v2.3)
 pip install -q opencv-python Pillow numpy deep-translator manga-ocr gradio
 
-# 수정된 폰트 다운로드 URL
 wget https://github.com/google/fonts/raw/main/ofl/nanumgothic/NanumGothic-Bold.ttf -O /content/NanumGothicBold.ttf
